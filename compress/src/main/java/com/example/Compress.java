e3d20e974016a4308995dbf534a06f7e8bf9b171
Zhou <1262202937@qq.com>
2017-10-23 15:12:33 +0800

    【整理压缩脚本】

--- compress/src/main/java/com/example/Compress.java
+++ compress/src/main/java/com/example/Compress.java
@@ -4,6 +4,7 @@ import com.tinify.Source;
 import com.tinify.Tinify;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -15,21 +16,24 @@ public class Compress {
     //每个key每个月可压缩500张
     private static String[] apiKey = {"7qq8sA6WBdp6VCiT3_FZ9YlqcvmAoeL7", "RlBBezjkDfJDiPOWN7u247YvBFHUYT0D", "opatDW9tpoZN5J030v_pfYYWQcpYTvgw"};
     private static String ModuleName = "compress";
+    private static final int dur = 5; //每dur张图片开一个线程
     private static int begin = 0;
+    private static int threadNum = 0;
+    private static long all_old_size;
+    private static long all_new_size;
 
     public static void main(String[] args) {
         try {
             Tinify.setKey(apiKey[0]);
-            Tinify.validate();
-            int compressionsThisMonth = Tinify.compressionCount();
-            System.out.println("此key本月已压缩:" + compressionsThisMonth);
+//            Tinify.validate(); //这句话太浪费时间了！！！
+//            int compressionsThisMonth = Tinify.compressionCount();
+//            System.out.println("此key本月已压缩:" + compressionsThisMonth);
         } catch (java.lang.Exception e) {
             System.out.println(e);
             return;
         }
         File[] files = null;
 //            Tinify.setProxy("http://user:pass@192.168.0.1:8080");
-        final int dur = 10;
         String path = Compress.class.getResource("").toString();
         path = "/" + path.substring(path.indexOf("/") + 1);
         path = path.substring(0, path.lastIndexOf(ModuleName) + ModuleName.length());
@@ -52,17 +56,26 @@ public class Compress {
         }
         int length = files != null ? files.length : 0;
         System.out.println("共需压缩:" + length + "张图片");
+        threadNum = length / dur + (length % dur == 0 ? 0 : 1);
         while (begin < length) {
             final int finalBegin = begin;
             final File[] finalFiles = files;
-            Thread thread = new Thread(new Runnable() {
+            final Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
                         for (int i = finalBegin; i < finalBegin + dur && i < finalFiles.length; i++) {
+                            long oldSize = getSize(finalFiles[i]);
+                            all_old_size += oldSize;
                             Source source = Tinify.fromFile(finalFiles[i].getPath());
                             source.toFile(new_path + finalFiles[i].getName());
-                            System.out.println(i + "：" + finalFiles[i].getName() + "   OK");
+                            long newSize = getSize(new File(new_path + finalFiles[i].getName()));
+                            System.out.println(i + "：" + finalFiles[i].getName() + "     " + oldSize / 1000.0 + "kb ==> " + newSize / 1000.0 + "kb");
+                            all_new_size += newSize;
+                        }
+                        threadNum--;
+                        if (threadNum == 0) {
+                            System.out.println("压缩完成, 共节省：" + (all_old_size - all_new_size) / 1000.0 + "kb");
                         }
                     } catch (IOException e) {
                         System.out.print(e);
@@ -72,5 +85,16 @@ public class Compress {
             thread.start();
             begin += dur;
         }
+    }
+
+    private static long getSize(File f) {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(f);
+            return fis.available();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return 0;
     }
 }
